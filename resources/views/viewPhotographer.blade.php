f0628c6e35c520eba6dc790334a4f69c9224a0b5
I Made Ganal Asmara Jaya <71816034+ganalasmara@users.noreply.github.com>
2020-12-09 16:03:52 +0800

    added viewPhotographer & added new data in seed

--- resources/views/viewPhotographer.blade.php
+++ resources/views/viewPhotographer.blade.php
@@ -1,22 +1,67 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/main.css">
     <title>Our Photographers</title>
 </head>
 <body>
-    <a href="{{url('/ourPhotographers')}}">Find a Photographer</a>
-    <a href="">Tour list</a>
-    <a href="">Login</a>
-    <a href="">Register</a>
-    @foreach($photographerList as $list)
-        <img src="{{$list->photographer_image}}">
-        <p>{{$list->name}}</p>
-        <p>{{$list->description}}</p>
-        <a href="{{url('/detailPhotographer/' . $list->photographer_id)}}">View Me</a>
-    @endforeach
+<div class="bg-photo">
+        <nav class="navbar fixed-top navbar-custom navbar-expand-lg navbar-light">
+            <div class="container">
+                <a class="navbar-brand logo" href="/"><img src="/Images/logo.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="ourPhotographers">Find a Photographer</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Tour List</a>
+                    </li>
+                    <li class="n  av-item">
+                    <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                    </li>
+
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+    
+        <div class="container" style="padding-top: 150px">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="maintext-photo">Our Photographers</p>
+                </div>
+                @foreach($photographerList as $list)
+                <div class="col-3">
+                    <div class="card mb-3" >
+                        <img class="card-img-top" src="{{$list->photographer_image}}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$list->name}}</h5>
+                                <p class="card-text">{{$list->description}}</p>
+                                <a href="{{url('/detailPhotographer/' . $list->photographer_id)}}" class="btn btn-primary">View Me</a>
+                            </div>
+                    </div>
+                </div>
+                @endforeach
+                
+            </div>
+        </div>
+</div>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,73 +1,74 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+<head>
 
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/css/main.css">
-    <title>Homepage</title>
-  </head>
-  
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
- 
-  <body>
-    <div class="bg">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/css/main.css">
+  <title>Homepage</title>
+</head>
+
+
+
+<body>
+  <div class="bg-main">
     <nav class="navbar fixed-top navbar-custom navbar-expand-lg navbar-light">
-        <div class="container">
+      <div class="container">
         <a class="navbar-brand logo" href="/"><img src="/Images/logo.svg" alt=""></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
-      
+
         <div class="collapse navbar-collapse " id="navbarSupportedContent">
           <ul class="navbar-nav ml-auto">
             <li class="nav-item">
               <a class="nav-link" href="ourPhotographers">Find a Photographer</a>
-            </li>            
+            </li>
             <li class="nav-item">
-                <a class="nav-link" href="#">Tour List</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Login</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Register</a>
-              </li>
-            
+              <a class="nav-link" href="#">Tour List</a>
+            </li>
+            <li class="n  av-item">
+              <a class="nav-link" href="#">Login</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Register</a>
+            </li>
+
           </ul>
-          
         </div>
-        </div>
+      </div>
     </nav>
 
-    
-    <div class="row align-items-center"></div>
-        <div class="container">
-            <div class="col mt-5">
-                <p class="maintext text-center" style="font-size: 50pt;">Graphy House</p>
-                <p class="maintext text-center" style="font-size: 30pt;">フォトグラフィー</p>
-                <div class="text-center mt-5">
-                <button type="button" class="btn btn-outline-secondary">Plan a Tour!</button> <br><br>
-                <a href="ourPhotographers"> <button type="button" class="btn btn-outline-secondary">Book a Photographer</button></a>
-                </div>
-            </div>
+
+    <div class="row align-items-center h-100">
+      <div class="container">
+        <div class="col mt-5">
+          <p class="maintext text-center" style="font-size: 70pt;">Graphy House</p>
+          <p class="maintext text-center" style="font-size: 30pt;">フォトグラフィー</p>
+          <div class="text-center mt-5">
+            <button type="button" class="btn btn-secondary">Plan a Tour!</button>
+            <a href="ourPhotographers"> <button type="button" class="btn btn-outline-secondary">Book a Photographer</button></a>
+          </div>
         </div>
+      </div>
     </div>
-    
+  </div>
 
-    
-    
-    <!-- JavaScript -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 
-    </div>
-  </body>
+
+
+  <!-- JavaScript -->
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+  </div>
+</body>
 
 </html>
