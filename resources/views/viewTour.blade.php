74ae826a6cd98a1b5a6b6f3e638e910eff793cb3
LiusKeny <72737613+LiusKeny@users.noreply.github.com>
2020-12-11 23:33:44 +0700

    view tour and view tour detail

--- resources/views/viewTour.blade.php
+++ resources/views/viewTour.blade.php
@@ -1,27 +1,68 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/main.css">
     <title>Our Tours</title>
 </head>
 <body>
-    <a href="{{url('/ourPhotographers')}}">Find a Photographer</a>
-    <a href="{{ Route('viewTour') }}">Tour List</a>
-    <a href="">Login</a>
-    <a href="">Register</a>
-    <br>
-    <br>
-    @foreach($tours as $tour)
-        <img src="{{$tour->tour_image}}" style="height:180px; width:286px;">
-        <p>{{$tour->name}}</p>
-        <p>{{$tour->description}}</p>
-        <a href="{{ Route('viewTourDetail', $tour->tour_id) }}">View Tour Details</a>
-        <br>
-        <br>
-    @endforeach
-    {{ $tours->links() }}
+<div class="bg-tour">
+        <nav class="navbar fixed-top navbar-custom navbar-expand-lg navbar-light">
+            <div class="container">
+                <a class="navbar-brand logo" href="/"><img src="/Images/logo.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="{{url('/ourPhotographers')}}">Find a Photographer</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="{{ Route('viewTour') }}">Tour List</a>
+                    </li>
+                    <li class="n  av-item">
+                    <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                    </li>
+
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+    
+        <div class="container" style="padding-top: 150px">
+            <div class="row">
+                <div class="col-md-12">
+                    <p class="maintext-photo">Tour Package</p>
+                </div>
+                @foreach($tours  as $tour)
+                <div class="col-4">
+                    <div class="card mb-3" >
+                        <img class="card-img-top-tour" src="{{$tour->tour_image}}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">{{$tour->name}}</h5>
+                                <p class="card-text">{{$tour->description}}</p>
+                                <a href="{{ Route('viewTourDetail', $tour->tour_id) }}" class="btn btn-primary">View Me</a>
+                            </div>
+                    </div>
+                </div>
+                @endforeach
+                {{ $tours->links() }}
+                
+            </div>
+        </div>
+</div>
 </body>
 </html>
--- resources/views/viewTourDetail.blade.php
+++ resources/views/viewTourDetail.blade.php
@@ -1,26 +1,71 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Our Tours</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="/css/main.css">
+    <title>Tours Detail</title>
 </head>
 <body>
-    <a href="{{url('/ourPhotographers')}}">Find a Photographer</a>
-    <a href="{{ Route('viewTour') }}">Tour List</a>
-    <a href="">Login</a>
-    <a href="">Register</a>
-    <br>
-    <br>
-    @foreach($details as $detail)
-        <h1><b>{{ $detail->name }}</b></h1>
-        <img src="{{$detail->tour_image}}" style="height:400px; width:600px;">
-        <h4>{{$detail->description}}</h4>
-        <h4>Starts from Rp.{{ $detail->price }}/pax <b></b><h4>
-        <a href="#">Book a Date!</a>
-        <br>
-    @endforeach
+<div class="bg-tour">
+        <nav class="navbar fixed-top navbar-custom navbar-expand-lg navbar-light">
+            <div class="container">
+                <a class="navbar-brand logo" href="/"><img src="/Images/logo.svg" alt=""></a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse " id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                    <a class="nav-link" href="{{url('/ourPhotographers')}}">Find a Photographer</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="{{ Route('viewTour') }}">Tour List</a>
+                    </li>
+                    <li class="n  av-item">
+                    <a class="nav-link" href="#">Login</a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="nav-link" href="#">Register</a>
+                    </li>
+
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+    
+        <div class="container" style="padding-top: 150px">
+            <div class="row">
+                @foreach($details as $detail)
+                <div class="col-md-12">
+                    <h1 class="tour-detail-title"><b>{{ $detail->name }}</b></h1>
+                </div>
+                    <div>
+                        <img src="{{$detail->tour_image}}" class="img-tour-detail" >
+                    </div>
+                    <div class="tour-detail-sec1">
+                        <div class="tour-detail-desc">
+                                {{$detail->description}}
+                        </div>
+                        <div>
+                            <p class="tour-detail-price">
+                                Starts from Rp.{{ $detail->price }}/pax <br>
+                                <a href="#">Book a Date!</a>
+                            </p>
+                        </div>
+                    </div>
+                    <br>
+                @endforeach
+            </div>
+        </div>
+</div>
 </body>
 </html>
